1361d1aedd2e4847542af9dc308b3c7a51f6ff03
Livvie Lin <livvie.lin@gmail.com>
2015-11-27 09:49:28 -0800

    Set up histogram classifier class

--- Luxocator/HistogramClassifier.py
+++ Luxocator/HistogramClassifier.py
@@ -2,3 +2,20 @@ import numpy
 import cv2
 import scipy.io
 import scipy.sparse
+
+class HistogramClassifier(object):
+
+  def __init__(self):
+
+    # controls level of logging
+    self.verbose = False
+    # public float that defines similarity threshold
+    # if all average similarities fall below, query image given unknown classification
+    self.minimumSimilarityForPositiveLabel = 0.075
+
+    # variables related to color space
+    self._channels = range(3)
+    self._histSize = [256] * 3
+    self._ranges = [0, 255] * 3
+    # dictionary to map string keys to lists of reference histograms
+    self._references = {}
