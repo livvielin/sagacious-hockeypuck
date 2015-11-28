e9c6c429132e1bfd26b6a9addf3d0cfb260a2150
Livvie Lin <livvie.lin@gmail.com>
2015-11-28 14:49:24 -0800

    Add function to histogram classifier for creating normalized histogram

--- Luxocator/HistogramClassifier.py
+++ Luxocator/HistogramClassifier.py
@@ -19,3 +19,17 @@ class HistogramClassifier(object):
     self._ranges = [0, 255] * 3
     # dictionary to map string keys to lists of reference histograms
     self._references = {}
+
+    # function to create normalized histogram
+    # optionally converts histogram to sparse format
+    def _createNormalizedHist(self, image, sparse):
+        # Create the histogram
+        hist = cv2.calcHist([image], self._channels, None, self._histSize, self._ranges)
+        # Normalize the histogram
+        hist[:] = hist * (1.0 / numpy.sum(hist))
+        # Convert the histogram to one column for efficient storage
+        hist = hist.reshape(16777216, 1)
+        if sparse:
+            #Conver the histogram to a sparse matrix
+            hist = scipy.sparse.csc_matrix(hist)
+        return hist
